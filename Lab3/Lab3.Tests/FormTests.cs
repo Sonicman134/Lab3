6ad4825c71276f762fd637724209e66c968bedd6
Sonicman134 <108946863+Sonicman134@users.noreply.github.com>
2023-03-17 13:29:55 +0700

    Created project

---
+++ Lab3/Lab3.Tests/FormTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Lab3.Tests
+{
+    [TestClass]
+    public class FormTests
+    {
+        [TestMethod]
+        public void Create()
+        {
+            Form1 form = new Form1();
+            Assert.IsNotNull(form);
+        }
+    }
+}
---
+++ Lab3/Lab3.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab3.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab3.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("683a300b-e627-4df4-977b-cbda13b3e83f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
