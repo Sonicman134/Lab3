91eec1591a2bd0311ba973a9af15522e4dcebb35
Sonicman134 <108946863+Sonicman134@users.noreply.github.com>
2023-03-17 17:14:05 +0700

    Added Game and Player classes

---
+++ Lab3/Lab3.Tests/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void Create()
+        {
+            Game game = new Game();
+            Assert.IsNotNull(game);
+        }
+    }
+}
---
+++ Lab3/Lab3/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Game
+    {
+        List<Player> players = new List<Player>();
+        MusicPlayer musicPlayer = new MusicPlayer();
+    }
+}
