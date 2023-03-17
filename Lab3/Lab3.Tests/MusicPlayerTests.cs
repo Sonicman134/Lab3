d0a48a4f5f4e7ee0e138e8a5433ab0fd236c52d5
Sonicman134 <108946863+Sonicman134@users.noreply.github.com>
2023-03-17 13:39:32 +0700

    Created music player

---
+++ Lab3/Lab3.Tests/MusicPlayerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Tests
+{
+    [TestClass]
+    public class MusicPlayerTests
+    {
+        [TestMethod]
+        public void Create()
+        {
+            MusicPlayer musicPlayer = new MusicPlayer();
+            Assert.IsNotNull(musicPlayer);
+        }
+    }
+}
